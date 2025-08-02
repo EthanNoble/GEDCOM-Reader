a0c754697b4e6455a94565c6a5efe67df43ba44c
EthanNoble <>
2025-08-02 03:02:19 -0500

    adsfasdf

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+'''
+This file is part of the GEDCOM-Reader project.
+'''
+
 from src.file import File
-import src.enums as enums
+from src import enums
 
 gedcom_file = File('')
 json_str = gedcom_file.jsonify(enums.JSONField.IND, enums.JSONField.FAM)
-with open('', 'w') as fp:
+with open('', 'w', encoding='utf-8') as fp:
     fp.write(json_str)
