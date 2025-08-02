5617d6232dcbd0accab33656463f263947a01bef
EthanNoble <>
2025-08-01 22:22:55 -0500

    adding config and fixing utils.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from src.file import File
 import src.enums as enums
 
-gedcom_file = File('ABSOLUTE_PATH_TO_GEDCOM_FILE')
+gedcom_file = File('/home/ethan/Desktop/GEDCOM-to-JSON/GedcomToJson/files/royal92.ged')
 gedcom_file.jsonify(True, enums.JSONField.IND, enums.JSONField.FAM)
