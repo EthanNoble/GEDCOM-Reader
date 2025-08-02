a4a2e0f9770c4a0d7d5765b153a79172aed87265
EthanNoble <>
2025-08-02 03:00:22 -0500

    dasfadsf

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ import src.enums as enums
 
 gedcom_file = File('')
 json_str = gedcom_file.jsonify(enums.JSONField.IND, enums.JSONField.FAM)
-with open('shakespeare.json', 'w') as fp:
+with open('', 'w') as fp:
     fp.write(json_str)
